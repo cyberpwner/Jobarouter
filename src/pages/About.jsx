948d8f764c9a35870e8a2851877b9ad502ee9348
cyberpwner <23579457+cyberpwner@users.noreply.github.com>
2024-04-17 21:58:37 +0200

    Using the Navigate Component to redirect users based on some conditions. Course Finished.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,13 @@
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
+
 function About() {
+  const [user, setUser] = useState('mario');
+
+  if (!user) {
+    return <Navigate to="/" replace />;
+  }
+
   return (
     <div className="home">
       <h2>About Us</h2>
@@ -21,6 +30,10 @@ function About() {
         non odit saepe facere voluptas esse mollitia illo fuga exercitationem id
         dicta iusto eaque numquam quaerat ad! Fugit velit beatae laudantium.
       </p>
+
+      <button type="button" onClick={() => setUser(null)}>
+        Logout
+      </button>
     </div>
   );
 }
