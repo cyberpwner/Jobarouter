a92ab92e9312925ada58a0c5faa025648072d120
cyberpwner <23579457+cyberpwner@users.noreply.github.com>
2024-04-17 21:40:47 +0200

    Forms and Actions in React Router

--- src/pages/help/Contact.jsx
+++ src/pages/help/Contact.jsx
@@ -1,8 +1,12 @@
+import { Form, redirect, useActionData } from 'react-router-dom';
+
 function Contact() {
+  const data = useActionData();
+
   return (
     <div className="contact">
       <h3>Contact Us</h3>
-      <form>
+      <Form method="POST" action="/help/contact">
         <label htmlFor="email">
           <span>Your email:</span>
           <input type="email" name="email" id="email" required />
@@ -12,9 +16,28 @@ function Contact() {
           <textarea name="message" id="message" required />
         </label>
         <button type="submit">Submit</button>
-      </form>
+
+        {data && data.error && <small className="error">{data.error}</small>}
+      </Form>
     </div>
   );
 }
 
 export default Contact;
+
+export const contactAction = async ({ request }) => {
+  const data = await request.formData();
+
+  const submission = {
+    email: data.get('email'),
+    message: data.get('message'),
+  };
+
+  if (submission.message.trim().length < 10) {
+    return {
+      error: 'Message must have at least 10 characters',
+    };
+  }
+
+  return redirect('/');
+};
